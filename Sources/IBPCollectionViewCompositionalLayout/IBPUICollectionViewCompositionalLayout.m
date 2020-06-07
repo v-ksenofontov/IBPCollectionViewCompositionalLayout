8dbebb2269ac777f628e8bd0f082bab91dab84d6
Ruslan Timchenko <rustimchek@mail.ru>
2020-06-08 01:15:55 +0300

    Small fix to the previous commit

--- Sources/IBPCollectionViewCompositionalLayout/IBPUICollectionViewCompositionalLayout.m
+++ Sources/IBPCollectionViewCompositionalLayout/IBPUICollectionViewCompositionalLayout.m
@@ -366,7 +366,8 @@
                         }
                         for (IBPCollectionViewOrthogonalScrollerSectionController *controller in orthogonalScrollerSectionControllers.allValues) {
                             CGRect frame = controller.scrollView.frame;
-                            if ((CGRectGetMinY(frame) - CGRectGetMinY(itemFrame)) >= -1.f) {
+                            BOOL needToAddExtendHeightValue = (CGRectGetMinY(frame) - CGRectGetMinY(itemFrame));
+                            if ((needToAddExtendHeightValue >= -1.f) || (needToAddExtendHeightValue <= 1.f)) {
                                 frame.origin.y += extendHeight;
                                 controller.scrollView.frame = frame;
                                 contentFrame = CGRectUnion(contentFrame, frame);
